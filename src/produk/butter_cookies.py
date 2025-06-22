c83b89f4e2141049d0f8fd22e2881cc7415d9ab4
gigahidro <gigahidromadani@gmail.com>
2025-06-22 17:16:42 +0700

    ubah BC

--- src/produk/butter_cookies.py
+++ src/produk/butter_cookies.py
@@ -3,4 +3,16 @@ from ..interfaces import BisaDiberiToping
 
 class ButterCookies(ProdukRoti, BisaDiberiToping):
     inisial = "BC"
-    pass
+    def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int, bahan_baku):
+        super().__init__(nama_produk, kode_produk, biaya_produksi, harga_jual, ukuran_batch)
+        self.bahan_baku = bahan_baku
+        
+    def toping(self):
+        """Metode untuk menambahkan toping pada Butter Cookies."""
+        return "Menambahkan toping pada Butter Cookies."
+    
+    def get_info(self) -> dict:
+        """Mengembalikan informasi produk Butter Cookies."""
+        info = super().get_info()
+        info["Toping"] = "Dapat diberi toping"
+        return info
