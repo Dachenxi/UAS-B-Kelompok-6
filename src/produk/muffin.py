89c9df5860a8d32f050ea2fe06bc850cd8071b9c
Ryan <ryanivan1306@gmail.com>
2025-06-22 18:25:31 +0700

    muffin dan roti manis

--- src/produk/muffin.py
+++ src/produk/muffin.py
@@ -3,4 +3,21 @@ from ..interfaces import BisaDikembangkan, BisaDiberiToping
 
 class Muffin(ProdukRoti, BisaDikembangkan, BisaDiberiToping):
     inisial = "MF"
-    pass
+    def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int, bahan_baku):
+        super().__init__(nama_produk, kode_produk, biaya_produksi, harga_jual, ukuran_batch)
+        self.bahan_baku = bahan_baku
+        
+    def pengembangan(self):
+        """Metode untuk proses pengembangan pada Muffin."""
+        return "Proses pengembangan pada Muffin."
+    
+    def toping(self):
+        """Metode untuk menambahkan toping pada Muffin."""
+        return "Menambahkan toping pada Muffin."
+    
+    def get_info(self) -> dict:
+        """Mengembalikan informasi produk Muffin."""
+        info = super().get_info()
+        info["Pengembangan"] = "Dapat di kembangkan"
+        info["Toping"] = "Dapat diberi toping"
+        return info
