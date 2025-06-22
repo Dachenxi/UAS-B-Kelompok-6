0942611fec26d03e09961a0df38373edc0355f88
gigahidro <gigahidromadani@gmail.com>
2025-06-23 00:37:51 +0700

    Memperbaiki BC dan CR

--- src/produk/butter_cookies.py
+++ src/produk/butter_cookies.py
@@ -3,16 +3,20 @@ from ..interfaces import BisaDiberiToping
 
 class ButterCookies(ProdukRoti, BisaDiberiToping):
     inisial = "BC"
-    def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int, bahan_baku):
+    def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int):
         super().__init__(nama_produk, kode_produk, biaya_produksi, harga_jual, ukuran_batch)
-        self.bahan_baku = bahan_baku
+        self.bahan_baku = {
+            'Tepung Terigu': '200 gr',
+            'Mentega': '100 gr',
+            'Gula Pasir': '50 gr',
+            'Telur': '1 butir',
+            'Vanili': '1 sdt',
+        }
         
     def toping(self):
-        """Metode untuk menambahkan toping pada Butter Cookies."""
         return "Menambahkan toping pada Butter Cookies."
     
     def get_info(self) -> dict:
-        """Mengembalikan informasi produk Butter Cookies."""
         info = super().get_info()
         info["Toping"] = "Dapat diberi toping"
         return info
--- src/produk/croissant.py
+++ src/produk/croissant.py
@@ -3,4 +3,21 @@ from ..interfaces import BisaDikembangkan
 
 class Croissant(ProdukRoti, BisaDikembangkan):
     inisial = "CR"
-    pass
+    def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int):
+        super().__init__(nama_produk, kode_produk, biaya_produksi, harga_jual, ukuran_batch)
+        self.bahan_baku = {
+            'Tepung Terigu': '250 gr',
+            'Mentega': '150 gr',
+            'Gula Pasir': '50 gr',
+            'Garam': '1 sdt',
+            'Ragi': '1 sdt',
+            'Susu Cair': '100 ml',
+        }
+
+    def pengembangan(self):
+        return "Adonan croissant dikembangkan dengan teknik lipatan berlapis untuk menghasilkan tekstur yang renyah."
+    
+    def get_info(self) _> dict:
+        info = super().get_info()
+        info["Pengembangan"] = "Dapat dikembangkan dengan teknik lipatan berlapis untuk menghasilkan tekstur yang renyah."
+        return info
