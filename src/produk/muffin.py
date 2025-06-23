b936c1f7da152213db65f348410df3a8da5356c9
Ryan <ryanivan1306@gmail.com>
2025-06-23 07:46:56 +0700

    muffin dan roti manis

--- src/produk/muffin.py
+++ src/produk/muffin.py
@@ -5,11 +5,27 @@ class Muffin(ProdukRoti, BisaDikembangkan, BisaDiberiToping):
     inisial = "MF"
     def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int, bahan_baku):
         super().__init__(nama_produk, kode_produk, biaya_produksi, harga_jual, ukuran_batch)
-        self.bahan_baku = bahan_baku
-        
+        self.bahan_baku = {
+            'Telur': '4 butir',
+            'Gula': '200 gr',
+            'Susu cair': '200 ml',
+            'Minyak': '200 ml',
+            'Terigu': '625 gram',
+            'Vanili': '1 sachet',
+            'Backing powder': '15 gram',
+        }
+    
+    def pengadonan(self):
+        """Metode untuk proses pengadonan pada Muffin"""
+        return "Proses Pengadonan pada bahan Muffin"
+    
     def pengembangan(self):
         """Metode untuk proses pengembangan pada Muffin."""
         return "Proses pengembangan pada Muffin."
+    
+    def pemanggangan(self):
+        """Metode untuk proses pemanggangan pada Muffin"""
+        return "Proses pemanggangan pada Muffin"
     
     def toping(self):
         """Metode untuk menambahkan toping pada Muffin."""
--- src/produk/roti_manis.py
+++ src/produk/roti_manis.py
@@ -5,11 +5,29 @@ class RotiManis(ProdukRoti, BisaDikembangkan):
     inisial = "RM"
     def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int, bahan_baku):
         super().__init__(nama_produk, kode_produk, biaya_produksi, harga_jual, ukuran_batch)
-        self.bahan_baku = bahan_baku
-        
+        self.bahan_baku = {
+            'Tepung terigu': '500 gram',
+            'Gula pasir': '100 gram',
+            'Susu bubuk': '25 gram',
+            'Kuning telur': '4 butir',
+            'Ragi instant': '11 gram',
+            'Susu cair': '130 ml',
+            'Air es': '100 ml',
+            'Mentega': '100 gram',
+            'Garam': '1 sdt',
+        }
+    
+    def pengadonan(self):
+        """Metode untuk proses pengadonan pada Roti Manis"""
+        return "Proses Pengadonan pada bahan Roti Manis"
+    
     def pengembangan(self):
         """Metode untuk proses pengembangan pada Roti Manis."""
         return "Proses pengembangan pada Roti Manis."
+    
+    def pemanggangan(self):
+        """Metode untuk proses pemanggangan pada Roti Manis"""
+        return "Proses pemanggangan pada Roti Manis"
     
     def get_info(self) -> dict:
         """Mengembalikan informasi produk Roti Manis."""
