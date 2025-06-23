6d7adf3c273aeeaa5cef89afa90a32069ca4414e
gigahidro <gigahidromadani@gmail.com>
2025-06-23 12:38:03 +0700

    Update muffin.py

--- src/produk/muffin.py
+++ src/produk/muffin.py
@@ -3,7 +3,7 @@ from ..interfaces import BisaDikembangkan, BisaDiberiToping
 
 class Muffin(ProdukRoti, BisaDikembangkan, BisaDiberiToping):
     inisial = "MF"
-    def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int, bahan_baku):
+    def __init__(self, nama_produk: str, kode_produk: str, biaya_produksi: int, harga_jual: int, ukuran_batch: int):
         super().__init__(nama_produk, kode_produk, biaya_produksi, harga_jual, ukuran_batch)
         self.bahan_baku = {
             'Telur': '4 butir',
